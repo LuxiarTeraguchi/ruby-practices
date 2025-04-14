f8b82f1a8fee5ecd548556d87f00be7050fd1bbd
LuxiarTeraguchi <fumika.teraguchi@luxiar.com>
2025-04-14 13:08:17 +0900

    fizzbuzzプログラムのレビュー部分修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,25 +1,17 @@
 #!/usr/bin/env ruby
-FizzBuzz = 15
-Fizz = 3
-Buzz = 5
-PrintStart = 1
-PrintEnd = 20
+PRINT_START = 1
+PRINT_END = 20
 
-# fizzbuzz判定・出力.
 def fizzbuzz(number)
-  # 3と5両方の倍数.
-  if number % FizzBuzz == 0
+  if number % 15 == 0
     puts "FizzBuzz"
-  # 3の倍数.
-  elsif number % Fizz == 0
+  elsif number % 3 == 0
     puts "Fizz"
-  # 5の倍数.
-  elsif number % Buzz == 0
+  elsif number % 5 == 0
     puts "Buzz"
   else
     puts number
   end
 end
 
-# PrintStartからPrintEndまでプリント.
-PrintStart.upto(PrintEnd) { |i| fizzbuzz(i)}
+PRINT_START.upto(PRINT_END) { |i| fizzbuzz(i) }
